a94d2b32a66386d5d8bc65a8511cdd479344ae78
datt30 <davidtamayo30@gmail.com>
2017-07-14 16:08:33 -0500

    first part of save img in store and more

--- app/negocioInterno.js
+++ app/negocioInterno.js
@@ -6,46 +6,30 @@ var formRegister_negocio_interno;
 
 //variable de urbanizacion
 var refUrbanizacion;
-//variable de negocios internos en refUrbanizacion
-var refUrbanizacion_negocio_interno;
-
-//variables para guardar imagen del negocio interno
-var storageRef;
-var uploadTask;
 
 
 function initialize(){
 formRegister_negocio_interno = document.getElementById('form_negocio_interno');
 formRegister_negocio_interno.addEventListener("submit",send_negocio_interno_toFirebase, false);
 
-//se toman las ramas de la unidad y sus negocios internos
 refUrbanizacion = firebase.database().ref().child("unidad x");
-refUrbanizacion_negocio_interno= refUrbanizacion.child("negocio_interno");
-
-storageRef = firebase.storage().ref('unidadx/');
-
 }
 
 
 
 function send_negocio_interno_toFirebase(event){
   event.preventDefault();
+  refUrbanizacion_negocio_interno= refUrbanizacion.child("negocio_interno");
 
-  //GUARDAMOS LA IMAGEN
-  var file= event.target.img.file;
-  storageRef.put(file);
-
-  //GUARDAMOS LOS DATOS
   refUrbanizacion_negocio_interno.push({
       nombre: event.target.nombre.value,
       descripcion: event.target.descripcion.value,
       telefono: event.target.telefono.value,
       celular: event.target.celular.value,
-      correo: event.target.correo.value,
-      pagina: event.target.pagina.value,
-      img1: uploadTask.snapshot.downloadURL
+      bloque: event.target.bloque.value,
+      NIT: event.target.nit.value,
+      img1: "img1_URL",
+      img2: "img2_URL"
   });
-
-
  formRegister_negocio_interno.reset();
 }
